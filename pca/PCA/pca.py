9a5023358a69fe30f349d5609157084853d0c2b3
Shivam Balwani <201851122@iiitvadodara.ac.in>
2021-10-11 22:34:12 +0530

    Added PCA

--- pca/PCA/pca.py
+++ pca/PCA/pca.py
@@ -1,1 +1,20 @@
-# class
+class PCA:
+  def __init__(self, image, variance):
+    self.variance = variance
+    self.img = image
+    self.dimension_m, self.dimension_n = np.shape(image)    
+    # self.mat = glob.glob('/content/drive/MyDrive/IVA_Project/*.pgm') # to read all image
+    # self.matrix = mpimg.imread(self.mat[0])
+
+  def covariance(A):
+    # find covariance matrix
+    cov=np.cov(A)
+
+  def eigen_val_vec(A):
+    # find eigen values and eigen vectors of matrix
+    eigval, eigvec = np.linalg.eig(A)
+
+  def plotting(A):
+    # plot graphs
+    plt.figure()
+    plt.plot(A)
