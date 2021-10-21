2ba0d60ae03c726057bd48bc334d0c27064ab483
theyashshahs <201851150@iiitvadodara.ac.in>
2021-10-21 21:56:07 +0530

    resolved merge conflicts

--- image_compressor/settings.py
+++ image_compressor/settings.py
@@ -119,9 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # media directory in the root directory
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")  # media directory in the root directory
+MEDIA_URL = "/media/"
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- pca/views.py
+++ pca/views.py
@@ -1,20 +1,23 @@
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 from rest_framework import response, status, views
-from django.core.files.storage import FileSystemStorage
 
 
-<<<<<<< HEAD
 class SaveFileView(views.APIView):
     def post(self, request, *args, **kwargs):
-        request_file = request.FILES['document'] if 'document' in request.FILES else None
+        request_file = (
+            request.FILES["document"] if "document" in request.FILES else None
+        )
         if request_file:
             fs = FileSystemStorage()
             file = fs.save(request_file.name, request_file)
             fileurl = fs.url(file)
-            return response.Response({"success":True}, status=status.HTTP_200_OK)
+            return response.Response({"success": True}, status=status.HTTP_200_OK)
         else:
-            return response.Response({"success":False}, status=status.HTTP_400_BAD_REQUEST)
-=======
+            return response.Response(
+                {"success": False}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+
 def index(request):
     return render(request, "pca/index.html")
->>>>>>> cec7f8a5a1f156915ebacea1e28854ec6eded5c9
