31bbb372cf1fd8a9184aad6075474341ca93ac28
Shivam Balwani <201851122@iiitvadodara.ac.in>
2021-10-24 18:02:17 +0530

    Code fixed

--- pca/PCA/convert.py
+++ pca/PCA/convert.py
@@ -18,8 +18,11 @@ def convert(jpg_file, y, x):
     jpg_file = os.path.join(settings.MEDIA_ROOT, jpg_file)
     jpg = Image.open(jpg_file).convert("L")
     jpg = jpg.resize((y, x), Image.BILINEAR)
-    file_name, _, _ = jpg_file.partition(".")
+    file_name, _, org_format = jpg_file.partition(".")
     name = file_name + ".pgm"
+    b_w=file_name+"."+org_format
     new_image = os.path.join(settings.MEDIA_ROOT, name)
+    new_image_b_w = os.path.join(settings.MEDIA_ROOT, b_w)
     jpg.save(new_image)
+    jpg.save(new_image_b_w)
     return name
